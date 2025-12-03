998e0ccb9f8f599a0208451ba05d523566e7ffb3
sadMaxim <dresvynnikov@gmail.com>
2025-12-03 13:21:14 +0100

    switch to other branch

--- flake.nix
+++ flake.nix
@@ -2,14 +2,12 @@
   description = "nixos";
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixvim.url = "github:nix-community/nixvim";
-    # pip2nix.url ="github:nix-community/pip2nix"; 
-    # ironbar.url = "github:nix-community/home-manager";
-    # ironbar.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
 
